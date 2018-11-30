84b0327cc0de60308152846fa097b354d0da491e
sfmiller940 <nineforty@gmail.com>
2018-11-29 22:38:26 -0800

    Basic testing

--- test/tictactoethTest.js
+++ test/tictactoethTest.js
@@ -9,52 +9,95 @@ contract('gameLib', (accounts)=>{
     return tictactoeth.deployed()
       .then((_instance)=>{
         instance = _instance;
-        return instance.fees.call()
+        return instance.fees.call();
       }).then((fees)=>{
         assert.equal( fees.toNumber(), 0, "Fees not 0");
-        return instance.numGames.call()
+        return instance.numGames.call();
       }).then((numGames)=>{
         assert.equal( numGames.toNumber(), 0, "Number of games not 0");
-        return instance.owner.call()
+        return instance.owner.call();
       }).then((owner)=>{
         assert.equal( owner, accounts[0], "Owner not initial account");
       });
   });
 
-/*
-  it("should create a new game", function() { // Upgrade to await?
-    return mothership.newGame( 200,300000000, 1, {from: accounts[1]} ).then((trx)=>{
-      return mothership.numGames.call().then((number)=>{
-        assert.equal(number.toNumber(),1,"Number of games not 1.");
-        return mothership.games.call(0).then((game)=>{
-          assert.equal(game[0],accounts[1],"Account 1 is not playerX.");
-          assert.equal(game[6].toNumber(),1,"Move number is not 1.");
-        });
+  it("should create a new game", function() {
+    var instance;
+    return tictactoeth.deployed()
+      .then((_instance)=>{
+        instance = _instance;
+        return instance.newGame.sendTransaction(123,247365,7,{value:456});
+      }).then((trx)=>{
+        return instance.numGames.call();
+      }).then((numGames)=>{
+        assert.equal( numGames.toNumber(), 1, "Number of games not 1");
+        return instance.games.call(0);
+      }).then((game)=>{
+          assert.equal(game[0],accounts[0],"playerX not account 0");
+          assert.equal(game[2].toNumber(),456,"Bet not 456");
+          assert.equal(game[3].toNumber(),123,"Wager not 123");
+          assert.equal(game[4].toNumber(),247365,"Turn length not 247365");
+          assert.equal(game[6].toNumber(),1,"Number of moves not 1");
+          return instance.getMoves.call(0)
+      }).then((moves)=>{
+          assert.equal(moves[7].toNumber(),1,"Move not set")
       });
-    });
   });
 
-  it("should join a second player",function(){
-    return mothership.joinGame.sendTransaction(0,3,{from:accounts[2],value:200}).then((trx)=>{
-      return mothership.games.call(0).then((game)=>{
-        assert.equal(game[6].toNumber(),2,"Move number is not 2.");
-        assert.equal(game[1],accounts[2],"Account 2 is not playerO.");
+  it("should join a second player", function() {
+    var id, instance;
+    return tictactoeth.deployed()
+      .then((_instance)=>{
+        instance = _instance;
+        return instance.newGame.sendTransaction(123,247365,7,{from:accounts[1],value:456});
+      }).then((trx)=>{
+        return instance.numGames.call();
+      }).then((numGames)=>{
+        id = numGames - 1;
+        return instance.joinGame.sendTransaction(id,6,{from:accounts[2],value:123});
+      }).then((trx)=>{
+        return instance.games.call(id);
+      }).then((game)=>{
+          assert.equal(game[0],accounts[1],"playerX not account 1");
+          assert.equal(game[1],accounts[2],"playerO not account 2");
+          assert.equal(game[2].toNumber(),456,"Bet not 456");
+          assert.equal(game[3].toNumber(),123,"Wager not 123");
+          assert.equal(game[4].toNumber(),247365,"Turn length not 247365");
+          assert.equal(game[6].toNumber(),2,"Number of moves not 2");
+          return instance.getMoves.call(id)
+      }).then((moves)=>{
+          assert.equal(moves[7].toNumber(),1,"playerX move not set")
+          assert.equal(moves[6].toNumber(),2,"playerO move not set")
       });
-    });
   });
 
-  it("should make a move",function(){
-    return mothership.newMove.sendTransaction(0,5,{from:accounts[1]}).then((trx)=>{
-      return mothership.games.call(0).then((game)=>{
-        assert.equal(game[6].toNumber(),3,"Move number is not 3.");
-        return mothership.getMoves.call(0).then((moves)=>{
-          assert.equal(moves[1],1,"First move incorrect.");
-          assert.equal(moves[3],2,"Second move incorrect.");
-          assert.equal(moves[5],1,"Third move incorrect.");
-        });
+  it("should make a move", function() {
+    var id, instance;
+    return tictactoeth.deployed()
+      .then((_instance)=>{
+        instance = _instance;
+        return instance.newGame.sendTransaction(123,247365,7,{from:accounts[1],value:456});
+      }).then((trx)=>{
+        return instance.numGames.call();
+      }).then((numGames)=>{
+        id = numGames - 1;
+        return instance.joinGame.sendTransaction(id,6,{from:accounts[2],value:123});
+      }).then((trx)=>{
+        return instance.newMove.sendTransaction(id,5,{from:accounts[1]});
+      }).then((trx)=>{
+        return instance.games.call(id);
+      }).then((game)=>{
+          assert.equal(game[0],accounts[1],"playerX not account 1");
+          assert.equal(game[1],accounts[2],"playerO not account 2");
+          assert.equal(game[2].toNumber(),456,"Bet not 456");
+          assert.equal(game[3].toNumber(),123,"Wager not 123");
+          assert.equal(game[4].toNumber(),247365,"Turn length not 247365");
+          assert.equal(game[6].toNumber(),3,"Number of moves not 3");
+          return instance.getMoves.call(id)
+      }).then((moves)=>{
+          assert.equal(moves[7].toNumber(),1,"playerX move not set")
+          assert.equal(moves[6].toNumber(),2,"playerO move not set")
+          assert.equal(moves[5].toNumber(),1,"New move not set")
       });
-    });
   });
-*/
-
 });
